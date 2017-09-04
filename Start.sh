a90a8caf3b6af10e75ad72e0537583327a8b3688
kirishima677 <nishioka.temp@gmail.com>
2017-09-04 22:30:46 +0900

    クライアントに不要なファイル群を削除

--- Start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#minecraft-server-forgeのスタート用のスクリプト作ってみるよー(´･ω･)っ
-#2017.6.1 起動可能であることを確認　(眞子)
-
-java -Xms4G -Xmx4G  -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:-UseAdaptiveSizePolicy -Xmn512M -XX:PermSize=512M -jar minecraft-server-forge.jar
-
